85e8e695523128a37bad0672f4b1e0f6e113c1fc
masa <iwamasa.0526.to@gmail.com>
2020-10-28 23:08:22 +0900

    if文の修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,45 @@
 import React, { useState } from "react";
-// import { Quiz } from "./components/Quiz";
 import "./styles.css";
+import { useForm } from "react-hook-form";
 
 export const App = () => {
+  const { register, handleSubmit, reset, errors } = useForm();
   const QUIZ = [
     {
       question: "日本の四季の正しい順番を答えて!",
-      correct: ["春", "夏", "秋", "冬"],
+      correct: ".*春.*夏.*秋.*冬.*",
     },
     {
       question: "正しい円周率を答えて！",
-      correct: ["3.14"],
+      correct: ".*3.14.*",
     },
     {
       question: "ジャンケンで相手がパーを出した時、こっちが勝てる手は?",
-      correct: ["チョキ"],
+      correct: ".*チョキ.*|.*ちょき.*",
     },
   ];
   const [questionTitle, setQuestionTitle] = useState(QUIZ[0].question);
   const [correct, setCorrect] = useState(QUIZ[0].correct);
-  const [answerText, setAnswerText] = useState("");
+  const [num, setNum] = useState(1);
+  let quizLength = QUIZ.length;
 
-  let num = 0;
-
-  const onChangeAnswerText = (e) => {
-    setAnswerText(e.target.value);
-  };
-
-  const onClickAdd = (e) => {
-    e.preventDefault();
-    if (answerText.match(QUIZ[num].correct)) {
-      correct = true;
+  const onClickAdd = (data) => {
+    const answer = data.text;
+    const reg = new RegExp(correct);
+    console.log(reg);
+    console.log(data);
+    console.log(num);
+    if (reg.test(answer)) {
+      alert("正解！！");
+      if (num < quizLength) {
+        setNum(num + 1);
+        setCorrect(QUIZ[num].correct);
+        setQuestionTitle(QUIZ[num].question);
+      } else {
+        alert("終了");
+      }
+      reset();
     }
-
-    if (correct) {
-      alert("正解");
-      num++;
-      setQuestionTitle(QUIZ[num].question);
-      setCorrect(QUIZ[num].correct);
-    }
-    if (answerText === "") {
-      alert("入力してください");
-    } else if (!correct) {
-      alert("不正解...");
-    }
-
-    setAnswerText("");
   };
 
   return (
@@ -55,13 +49,16 @@ export const App = () => {
       </div>
 
       <div className="answer-area">
-        <input
-          placeholder="解答を入力する"
-          value={answerText}
-          onChange={onChangeAnswerText}
-          className="text"
-        />
-        <button onClick={onClickAdd}>解答する</button>
+        <form onSubmit={handleSubmit(onClickAdd)}>
+          <input
+            placeholder="解答を入力する"
+            name="text"
+            className="text"
+            ref={register({ required: true })}
+          />
+          {errors.text && alert("解答を入力してください")}
+          <button>解答する</button>
+        </form>
       </div>
     </>
   );
